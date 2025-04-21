473d9862ad3d3f7b1c8b5594c99d517aaaf2b112
Prince Gbezeh <gbezehp@gmail.com>
2025-04-21 23:41:50 +0000

    feat:: add download button

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -29,4 +29,18 @@ const fileDetailsGet = async (req, res) => {
   res.render('fileDetails', { details: file })
 }
 
-module.exports = { fileUploadPost, fileDetailsGet }
+const fileDownloadGet = async (req, res) => {
+  const { filename } = req.params
+  const { name } = await db.files.getSingleFile(filename)
+
+  const filepath = path.resolve('uploads', name)
+
+  res.download(filepath, (err) => {
+    if (err) {
+      console.error('Download error:', err)
+      res.status(500).send('Could not download file.')
+    }
+  })
+}
+
+module.exports = { fileUploadPost, fileDetailsGet, fileDownloadGet }
--- src/routers/files.js
+++ src/routers/files.js
@@ -1,10 +1,10 @@
 const { Router } = require('express')
-const { fileUploadPost, fileDetailsGet } = require('../controllers/files')
+const { fileUploadPost, fileDetailsGet, fileDownloadGet } = require('../controllers/files')
 
 const router = Router()
 
 router.post('/upload', fileUploadPost)
-
+router.get('/:filename/download', fileDownloadGet)
 router.get('/:filename/details', fileDetailsGet)
 
 module.exports = router
