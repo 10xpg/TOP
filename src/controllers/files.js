678be5725207d4d0fa22493cd295491515c24e3f
Prince Gbezeh <gbezehp@gmail.com>
2025-04-23 12:14:58 +0000

    feat:: add logic to upload files to cloudstorage

--- src/controllers/files.js
+++ src/controllers/files.js
@@ -3,9 +3,13 @@ const multer = require('multer')
 const db = require('../db/queries')
 const path = require('path')
 const { filesize } = require('filesize')
+const uploadToCloudStorage = require('../utils/cloudinary')
+const axios = require('axios')
 
-const upload = multer({ dest: 'uploads/' })
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
+// !: THIS FEATURE SUPPORTS IMAGES AND PDF
 const fileUploadPost = [
   upload.array('uploadedfile'),
   asyncHandler((req, res, next) => {
@@ -14,7 +18,12 @@ const fileUploadPost = [
     const { dirname } = req.body
     console.log(files)
     files.forEach(async (f) => {
-      await db.files.uploadFile(userId, f.filename, path.extname(f.originalname), f.size, f.originalname, f.mimetype, dirname)
+      const ext = path.extname(f.originalname)
+      let type = 'image'
+      if (ext !== '.jpeg' || '.jpg' || '.png' || '.gif' || '.svg' || '.webp') type = 'raw'
+      const uploadResult = await uploadToCloudStorage(f.buffer, type)
+      const { secure_url } = uploadResult
+      await db.files.uploadFile(userId, secure_url, ext, f.size, f.originalname, f.mimetype, dirname)
     })
 
     res.redirect('/home')
@@ -30,17 +39,20 @@ const fileDetailsGet = async (req, res) => {
 }
 
 const fileDownloadGet = async (req, res) => {
-  const { filename } = req.params
-  const { name } = await db.files.getSingleFile(filename)
-
-  const filepath = path.resolve('uploads', name)
-
-  res.download(filepath, (err) => {
-    if (err) {
-      console.error('Download error:', err)
-      res.status(500).send('Could not download file.')
-    }
-  })
+  try {
+    const { filename } = req.params
+    const { name, mimetype, originalname } = await db.files.getSingleFile(filename)
+    // // const filepath = path.resolve('uploads', name)
+    // download file from cloudinary
+    // where fileUrl === name
+    const response = await axios.get(name, { responseType: 'stream' })
+    res.setHeader('Content-Disposition', `attachment; filename="${originalname}"`)
+    res.setHeader('Content-Type', mimetype)
+    response.data.pipe(res)
+  } catch (err) {
+    console.error('Download error:', err)
+    res.status(500).send('Could not download file.')
+  }
 }
 
 module.exports = { fileUploadPost, fileDetailsGet, fileDownloadGet }
